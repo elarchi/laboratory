2051cd2af9279769fbccc7ff93593a9fd3c50f99
elarchi <emilie.leury@gmail.com>
2022-05-04 00:09:04 +0100

    update airbnb react native training, installation of the navigation app.js

--- reactNative/airbnb/App.js
+++ reactNative/airbnb/App.js
@@ -1,147 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [userToken, setUserToken] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const handleLogin = async (event) => {
-    try {
-      event.preventDefault();
-      if (email && password) {
-        setErrorMessage("");
-        const response = await axios.post("http://backendURL/", {
-          //backend URL to update
-          email,
-          password,
-        });
-        console.log("response.data:", reponse.data);
-        //miss token part
-      } else {
-        setErrorMessage("Please fill all fields");
-      }
-    } catch (error) {
-      console.log(error.response);
-      console.log(error.message);
-      if (error.response.status === 400 || error.response.status === 401) {
-        setErrorMessage("Wrong email or password");
-      }
+  const setToken = async (token) => {
+    if (token) {
+      await AsyncStorage.setItem("userToken", token);
+    } else {
+      await AsyncStorage.removeItem("userToken");
     }
+    setUserToken(token);
   };
 
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      const userToken = await AsyncStorage.getItem("userToken");
+      setUserToken(userToken);
+      setIsLoading(false);
+    };
+    bootstrapAsync();
+  }, []);
+
+  if (isLoading === true) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      {/* main view */}
-
-      <View style={styles.logo__View}>
-        {/* logo view */}
-
-        <Image
-          source={require("./assets/logo.png")}
-          resizeMode="contain"
-          style={styles.Image}
-        />
-        <Text style={styles.p22_bold__Text}>Sign in</Text>
-      </View>
-
-      <View style={styles.inputs__View}>
-        {/* inputs view */}
-        <TextInput
-          value={email}
-          placeholder="email"
-          style={styles.Input}
-          textContentType="emailAddress"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <TextInput
-          value={password}
-          placeholder="password"
-          style={styles.Input}
-          secureTextEntry={true}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-      </View>
-
-      <View style={styles.btn__View}>
-        {/* button view */}
-
-        <Text style={styles.red__Text}>{errorMessage}</Text>
-        <TouchableOpacity style={styles.TouchableOpacity} onPress={handleLogin}>
-          <Text style={styles.p18__Text}>Sign in</Text>
-        </TouchableOpacity>
-        <Text style={styles.p14__Text}>No account? Register</Text>
-      </View>
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator></Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-around",
-  },
-  logo__View: {
-    alignItems: "center",
-    justifyContent: "space-between",
-    height: 150,
-  },
-  Image: {
-    height: 100,
-  },
-  inputs__View: {
-    justifyContent: "space-between",
-    height: 50,
-  },
-  Input: {
-    borderColor: "white",
-    borderBottomColor: "red",
-    borderWidth: 1,
-    width: 150,
-  },
-  btn__View: {
-    alignItems: "center",
-    justifyContent: "space-between",
-    height: 90,
-  },
-  TouchableOpacity: {
-    borderColor: "red",
-    borderWidth: 2,
-    borderRadius: 20,
-    height: 40,
-    width: 150,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  p22_bold__Text: {
-    fontSize: 22,
-    fontWeight: "bold",
-    color: "grey",
-  },
-  p18__Text: {
-    fontSize: 18,
-    color: "grey",
-  },
-  p14__Text: {
-    fontSize: 14,
-  },
-  red__Text: {
-    color: "red",
-  },
-});
