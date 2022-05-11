200c10f3efd6286c12c18e2ff4fcf11373653cc5
elarchi <emilie.leury@gmail.com>
2022-05-11 11:04:42 +0100

    list syntax

--- python/freecodecamp01/helloworld.py
+++ python/freecodecamp01/helloworld.py
@@ -1,5 +1,6 @@
 from math import *  # to use math functions
 
+# ---------------------------------------------------------------------------------
 # STRING
 print("Hello World")  # console.log
 print("Hello\nWorld")
@@ -11,10 +12,11 @@ print(len(phrase))  # phrase.length
 print(phrase[0])  # the character at the position 0 of the variable ===> H
 print(phrase.index("W"))  # .indexOf();
 print(phrase.replace("Hello", "Goodbye"))
-
+# ---------------------------------------------------------------------------------
 # BOOLEAN
-is_true = True  # boolean with a capital letter  # variable's name isn't like JS camel,  but use underscore.
-
+is_true = True  # boolean with a capital letter  # variable's name isn't like JS camel
+# but use underscore.
+# ---------------------------------------------------------------------------------
 # NUMBER
 print(3 + 4)
 my_number = 5
@@ -32,7 +34,7 @@ print(round(3.7))  # return 4
 print(floor(3.7))  # return 3
 print(ceil(3.7))  # return 4
 print(sqrt(36))  # return 6
-
+# ---------------------------------------------------------------------------------
 # INPUT
 name = input("Enter your name here: ")
 print("Hello " + name)
@@ -46,16 +48,24 @@ print(result)  # the result will convert each number into a string
 result2 = int(num1) + int(num2)  # the result will be a integer this time  ===> 3 and 5 => 8
 print(result2)
 
-result3 = float(num1)+float(num2)  # to use decimal number
+result3 = float(num1) + float(num2)  # to use decimal number
 print(result3)
-
+# ---------------------------------------------------------------------------------
 # let's create a mini game: Madlib
-
 color = input("Enter a color: ")
 plural_noun = input("Enter a plural noun: ")
 celebrity = input("Enter a celebrity: ")
 
-print("Roses are "+color)
-print(plural_noun+" are blue")
-print("I love "+celebrity)
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+# ---------------------------------------------------------------------------------
+# LIST
+friends = ["Lucas", "Juary", "Victor", "Alice", "Jayne", "Gabi"]
+print(friends)
+print(friends[1])  # Juary
+print(friends[-2])  # Alice
+print(friends[1:])  # Juary Victor
+print(friends[1:3])  # Juary Victor, not Alice
 
+friends[1] = "Mike"  # will update our list
